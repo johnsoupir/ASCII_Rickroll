ddc1e53b0aabdbe9b587f7d6f15e2bdc6aac2eea
John Soupir <johnsoupir@gmail.com>
2024-06-06 22:05:33 -0500

    Fixes rickroll

---
+++ C Rickroll/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "ASCII_Rickroll.h"
+
+int main(int argc, char *argv[]) {
+
+    //Place your code here
+    printf("Hello World!\n");
+
+    //Roll the ASCII Rickroll at 120ms per frame. 
+    roll(120); 
+
+    //Our work is done! Exit with a status of 0.
+    return 0;
+}
