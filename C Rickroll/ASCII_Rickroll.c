9f445e2a6cbcb0f7d317cf505a1ee2f169e86968
John Soupir <johnsoupir@gmail.com>
2024-06-06 21:48:39 -0500

    Rethinks where to store frames

--- C Rickroll/ASCII_Rickroll.c
+++ C Rickroll/ASCII_Rickroll.c
@@ -2,31 +2,28 @@
 #include "ASCII_Rickroll.h"
 #include <time.h>
 
+
 void delay(int milliseconds) {
     clock_t start_time = clock();
     while (clock() < start_time + milliseconds * (CLOCKS_PER_SEC / 1000));
 }
 
-void roll(const char *filename, int frameTime) {
-    FILE *file = fopen(filename, "r");
-
-    int line_chars = 96;
+void roll(int frameTime) {
+    
+    int line_chars = 95;
     int lines = 35;
-    int frames = 149;
+    int numFrames = 149;
+    int frameIndex = 0;
 
-    while(fgetc(file)!= 0x1B);
-
-    for (int frame = 1; frame <= frames; frame++) {
+    for (int frame = 1; frame < numFrames; frame++) {
         for (int i = 0; i <= lines; i++) {
-            for (int j = 0; j < line_chars; j++) {
-                // Read in each character.
-                char next = fgetc(file);
-                if (next == EOF) break;
-                printf("%c",next);
+            for (int j = 0; j <= line_chars; j++) {
+                // Print each character.
+                printf("%c",frames[frameIndex]);
+                frameIndex++;
             }
         }
         delay(frameTime);
     }
-    fclose(file);
-    printf("Hello\n");
+    printf("\n");
 }
